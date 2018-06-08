6e435b1462a441b5f81a12db14a40ad40b9ee5f6
marcinkozak-p <marcinkozak.p@gmail.com>
2018-06-08 14:52:12 +0200

    Adding basic js objects

---
+++ testTask.js
@@ -0,0 +1,14 @@
+import './duck-page-model'
+
+fixture `The test task`
+    .page(`https://duckduckgo.com/`);
+
+let page;
+
+test ('Success scenario', async t =>{
+
+});
+
+test ('Failure scenario', async t =>{
+
+});
